048e6beadf098e63ab16dfe9d9a2612bce5ffb3a
Chhaviigautam <chhavigautam171@gmail.com>
2025-07-06 19:38:22 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect(process.env.mongoDBURL)
-    .then(() => console.log('DB CONNECTED'))
+    try {
+        await mongoose.connect(process.env.mongoDBURL)
+        console.log("DB connected")
+    } 
+    catch (err) {
+        console.error("db error", err)
+        process.exit(1)
+    }
 }
