fdb91b6f4787391862a2a39f9847df32de688dcb
Chhaviigautam <chhavigautam171@gmail.com>
2025-07-06 19:24:44 +0530

    Second Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://gautamchhavi13:meow123@cluster0.zslnadg.mongodb.net/ResuMeow')
+    await mongoose.connect(process.env.mongoDBURL)
     .then(() => console.log('DB CONNECTED'))
 }
